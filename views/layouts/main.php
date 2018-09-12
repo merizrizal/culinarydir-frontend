64352faab727f78abb0415dfd509f1556ab27f6b
Mei Rizal <meriz.rizal@gmail.com>
2018-09-12 10:57:10 +0700

    update

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -52,7 +52,28 @@ AppAsset::register($this); ?>
         foreach ($this->params['beforeEndBody'] as $value) {
             $value();
         }
-    } ?>
+    }
+
+    $jscript = '
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId            : "' . Yii::$app->params['facebook']['clientId'] . '",
+                autoLogAppEvents : true,
+                xfbml            : true,
+                version          : "v3.1"
+            });
+        };
+
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, "script", "facebook-jssdk"));
+    ';
+
+    $this->registerJs($jscript); ?>
 
 <?php $this->endBody() ?>
 </body>
