4fda9d34c53dad831a21ef00f13745d572ed86fc
Herryawan Wiguna <wigunaherryawan@gmail.com>
2018-11-27 19:44:38 +0700

    Fix label

--- views/order/checkout.php
+++ views/order/checkout.php
@@ -70,7 +70,7 @@ $this->title = 'Checkout'; ?>
                                                         <table class="table table-responsive table-striped table-border checkout-table">
                                                             <tbody>
                                                                 <tr>
-                                                                    <th><?= Yii::t('app', 'Total') ?></th>
+                                                                    <th class="font-alt">Total</th>
                                                                     <td><?= Yii::$app->formatter->asCurrency($modelTransactionSession['total_price']) ?></td>
                                                                 </tr>
                                                             </tbody>
