8f04d64a74b08e44a983eb96511ff3fcfdec649b
AxelRj13 <axel.raharja1@gmail.com>
2018-12-12 16:55:39 +0700

    Penghilangan saved search sementara

--- views/user/user/_saved_search.php
+++
@@ -1,9 +0,0 @@
-<div class="row saved-search">
-    <div class="col-sm-12 col-xs-12">
-        <div class="box bg-white p-0">
-            <div class="box-content">
-                <p><?= Yii::t('app', 'Coming Soon') ?></p>
-            </div>
-        </div>
-    </div>
-</div>
