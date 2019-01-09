5e9ac965176532ace05264606813e55d3193d511
Mei Rizal <meriz.rizal@gmail.com>
2019-01-09 14:08:59 +0700

    Merapikan code

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -31,52 +31,52 @@ class OrderController extends base\BaseController
     public function actionCheckout()
     {
         $modelTransactionSession = TransactionSession::find()
-        ->joinWith([
-            'business',
-            'transactionItems' => function($query) {
-            
-            $query->orderBy(['transaction_item.id' => SORT_ASC]);
-            },
-            'transactionItems.businessProduct'
-                ])
-                ->andWhere(['transaction_session.user_ordered' => Yii::$app->user->getIdentity()->id])
-                ->andWhere(['transaction_session.is_closed' => false])
-                ->one();
+            ->joinWith([
+                'business',
+                'transactionItems' => function($query) {
                 
-                if (Yii::$app->request->post()) {
+                    $query->orderBy(['transaction_item.id' => SORT_ASC]);
+                },
+                'transactionItems.businessProduct'
+            ])
+            ->andWhere(['transaction_session.user_ordered' => Yii::$app->user->getIdentity()->id])
+            ->andWhere(['transaction_session.is_closed' => false])
+            ->one();
+                
+        if (Yii::$app->request->post()) {
+            
+            $modelTransactionSession->is_closed = true;
+            
+            if ($modelTransactionSession->save()) {
+                
+                $businessPhone = '62' . substr(str_replace('-', '', $modelTransactionSession['business']['phone3']), 1);
+                
+                $itemCount = count($modelTransactionSession['transactionItems']) - 1;
+                $messageOrder = 'Halo ' . $modelTransactionSession['business']['name'] . ',\nsaya ' . Yii::$app->user->getIdentity()->full_name . ' (via Asikmakan) ingin memesan:\n\n';
+                
+                foreach ($modelTransactionSession['transactionItems'] as $itemIndex => $dataTransactionItem) {
                     
-                    $modelTransactionSession->is_closed = true;
-                    
-                    if ($modelTransactionSession->save()) {
-                        
-                        $businessPhone = '62' . substr(str_replace('-', '', $modelTransactionSession['business']['phone3']), 1);
-                        
-                        $itemCount = count($modelTransactionSession['transactionItems']) - 1;
-                        $messageOrder = 'Halo ' . $modelTransactionSession['business']['name'] . ',\nsaya ' . Yii::$app->user->getIdentity()->full_name . ' (via Asikmakan) ingin memesan:\n\n';
-                        
-                        foreach ($modelTransactionSession['transactionItems'] as $itemIndex => $dataTransactionItem) {
-                            
-                            $messageOrder .= $dataTransactionItem['amount'] . 'x ' . $dataTransactionItem['businessProduct']['name'] . ' @' . Yii::$app->formatter->asCurrency($dataTransactionItem['price']);
-                            $messageOrder .= (!empty($dataTransactionItem['note'])) ? ' ' . $dataTransactionItem['note'] : '';
-                            $messageOrder .= ($itemCount !== $itemIndex) ? '\n\n' : '';
-                        }
-                        
-                        $messageOrder .= '\n\n' . 'Total: ' . Yii::$app->formatter->asCurrency($modelTransactionSession['total_price']);
-                        
-                        $messageOrder = str_replace('%5Cn', '%0A', urlencode($messageOrder));
-                        
-                        return $this->redirect('https://api.whatsapp.com/send?phone=' . $businessPhone . '&text=' . $messageOrder);
-                    } else {
-                        
-                        Yii::$app->session->setFlash('message', [
-                            'title' => 'Gagal Checkout',
-                            'message' => 'Terjadi kesalahan saat menyimpan data',
-                        ]);
-                    }
+                    $messageOrder .= $dataTransactionItem['amount'] . 'x ' . $dataTransactionItem['businessProduct']['name'] . ' @' . Yii::$app->formatter->asCurrency($dataTransactionItem['price']);
+                    $messageOrder .= (!empty($dataTransactionItem['note'])) ? ' ' . $dataTransactionItem['note'] : '';
+                    $messageOrder .= ($itemCount !== $itemIndex) ? '\n\n' : '';
                 }
                 
-                return $this->render('checkout', [
-                    'modelTransactionSession' => $modelTransactionSession
+                $messageOrder .= '\n\n' . 'Total: ' . Yii::$app->formatter->asCurrency($modelTransactionSession['total_price']);
+                
+                $messageOrder = str_replace('%5Cn', '%0A', urlencode($messageOrder));
+                
+                return $this->redirect('https://api.whatsapp.com/send?phone=' . $businessPhone . '&text=' . $messageOrder);
+            } else {
+                
+                Yii::$app->session->setFlash('message', [
+                    'title' => 'Gagal Checkout',
+                    'message' => 'Terjadi kesalahan saat menyimpan data',
                 ]);
+            }
+        }
+        
+        return $this->render('checkout', [
+            'modelTransactionSession' => $modelTransactionSession
+        ]);
     }
 }
