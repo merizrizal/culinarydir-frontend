84cf24fe2a25f0d07aa40aac52a2b5c0912852ee
Mei Rizal <meriz.rizal@gmail.com>
2019-01-26 16:17:19 +0700

    Perbaikan code

--- components/GrowlCustom.php
+++ components/GrowlCustom.php
@@ -18,47 +18,50 @@ class GrowlCustom extends Widget
     {
         return '
             function messageResponse(icon, title, message, type) {
+
+                if (title != "302") {
             
-                $.notify({
-                    icon: icon,
-                    title: title,
-                    message: message,
-                },{
-                    element: "body",
-                    position: null,
-                    type: type,
-                    allow_dismiss: true,
-                    newest_on_top: false,
-                    showProgressbar: false,
-                    placement: {
-                        from: "bottom",
-                        align: "left"
-                    },
-                    offset: {
-                        x: 0,
-                        y: ' . (Yii::$app->request->getUserAgent() == 'com.asikmakan.app' ? 60 : 0) . '
-                    },
-                    spacing: 0,
-                    z_index: 1031,
-                    delay: 5000,
-                    timer: 1000,
-                    animate: {
-                        enter: "animated fadeInDown",
-                        exit: "animated fadeOutUp"
-                    },
-                    template:
-                        "<div data-notify=\"container\" class=\"col-lg-3 col-md-4 col-sm-5 col-xs-12\">" +
-                            "<div class=\"alert alert-{0} mb-10\" style=\"\" role=\"alert\">" +
-                                "<button type=\"button\" aria-hidden=\"true\" class=\"close\" data-notify=\"dismiss\">×</button>" +
-                                "<span data-notify=\"icon\"></span> " +
-                                "<span data-notify=\"title\"><b>{1}</b></span><br>" +
-                                "<span data-notify=\"message\">{2}</span>" +
-                                "<div class=\"progress\" data-notify=\"progressbar\">" +
-                                    "<div class=\"progress-bar progress-bar-{0}\" role=\"progressbar\" aria-valuenow=\"0\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: 0%;\"></div>" +
+                    $.notify({
+                        icon: icon,
+                        title: title,
+                        message: message,
+                    },{
+                        element: "body",
+                        position: null,
+                        type: type,
+                        allow_dismiss: true,
+                        newest_on_top: false,
+                        showProgressbar: false,
+                        placement: {
+                            from: "bottom",
+                            align: "left"
+                        },
+                        offset: {
+                            x: 0,
+                            y: ' . (Yii::$app->request->getUserAgent() == 'com.asikmakan.app' ? 60 : 0) . '
+                        },
+                        spacing: 0,
+                        z_index: 1031,
+                        delay: 5000,
+                        timer: 1000,
+                        animate: {
+                            enter: "animated fadeInDown",
+                            exit: "animated fadeOutUp"
+                        },
+                        template:
+                            "<div data-notify=\"container\" class=\"col-lg-3 col-md-4 col-sm-5 col-xs-12\">" +
+                                "<div class=\"alert alert-{0} mb-10\" style=\"\" role=\"alert\">" +
+                                    "<button type=\"button\" aria-hidden=\"true\" class=\"close\" data-notify=\"dismiss\">×</button>" +
+                                    "<span data-notify=\"icon\"></span> " +
+                                    "<span data-notify=\"title\"><b>{1}</b></span><br>" +
+                                    "<span data-notify=\"message\">{2}</span>" +
+                                    "<div class=\"progress\" data-notify=\"progressbar\">" +
+                                        "<div class=\"progress-bar progress-bar-{0}\" role=\"progressbar\" aria-valuenow=\"0\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: 0%;\"></div>" +
+                                    "</div>" +
                                 "</div>" +
-                            "</div>" +
-                        "</div>"
-                });
+                            "</div>"
+                    });
+                }
             }
         ';
     }
