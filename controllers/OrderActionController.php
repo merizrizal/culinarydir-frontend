140a04b48f1dba07d3bb1afd91ffa16d6178db4a
AxelRj13 <axel.raharja1@gmail.com>
2019-04-09 18:06:57 +0700

    Perbaikan code

--- controllers/OrderActionController.php
+++ controllers/OrderActionController.php
@@ -115,9 +115,7 @@ class OrderActionController extends base\BaseController
         $post = Yii::$app->request->post();
 
         $modelTransactionItem = TransactionItem::find()
-            ->joinWith([
-                'transactionSession'
-            ])
+            ->joinWith(['transactionSession'])
             ->andWhere(['transaction_item.id' => !empty($post['id']) ? $post['id'] : null])
             ->one();
 
@@ -169,9 +167,7 @@ class OrderActionController extends base\BaseController
     public function actionRemoveItem()
     {
         $modelTransactionItem = TransactionItem::find()
-            ->joinWith([
-                'transactionSession'
-            ])
+            ->joinWith(['transactionSession'])
             ->andWhere(['transaction_item.id' => !empty(Yii::$app->request->post('id')) ? Yii::$app->request->post('id') : null])
             ->one();
 
