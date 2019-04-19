755d0f8179d310fdbebc42fb94290523784cd905
Mei Rizal <meriz.rizal@gmail.com>
2019-04-19 13:51:54 +0700

    Menghapus fungsi icheck

--- config/params.php
+++ config/params.php
@@ -24,14 +24,6 @@ return [
     'errMysql' => [
         '7' => '<br>Data ini terkait dengan data yang terdapat pada modul yang lain.',
     ],
-    'checkbox-radio-script' => function($type = null, $color = null, $element = 'input[type=\"radio\"], input[type=\"checkbox\"]') {
-        $jscript = '$("' . $element . '").iCheck({
-                    checkboxClass: "icheckbox_' . (!empty($type) ? $type : 'square') . '-' . (!empty($color) ? $color : 'red') . '",
-                    radioClass: "iradio_' . (!empty($type) ? $type : 'square') . '-' . (!empty($color) ? $color : 'red') . '"
-                });';
-
-        return $jscript;
-    },
     'pageSize' => 25,
     'subprogramLocal' => 'front',
 ];
