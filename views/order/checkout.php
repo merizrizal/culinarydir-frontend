95e0851e69df6e241a629517ddf10b4f7f9e3c79
AxelRj13 <axel.raharja1@gmail.com>
2019-04-23 15:38:59 +0700

    Perbaikan layout

--- views/order/checkout.php
+++ views/order/checkout.php
@@ -432,12 +432,12 @@ $this->title = 'Checkout'; ?>
 	<div class="row">
 		<div class="col-md-7 col-md-offset-4 col-sm-offset-3 col-sm-8 col-tab-12 col-xs-offset-1 col-xs-11">
 			<div class="row">
-        		<div class="col-xs-12">
+        		<div class="col-xs-11">
         			<div class="delivery-method"></div>
         		</div>
         	</div>
         	<div class="row">
-        		<div class="col-xs-12">
+        		<div class="col-xs-11">
         			<div class="payment-method"></div>
         		</div>
         	</div>
